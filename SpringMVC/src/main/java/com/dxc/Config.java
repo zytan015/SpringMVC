87bc2d3a405207e44c2e69d067450575061b5c09
joeytzy <106920142+zytan015@users.noreply.github.com>
2022-09-06 23:50:20 +0800

    Add files via upload

---
+++ SpringMVC/src/main/java/com/dxc/Config.java
@@ -0,0 +1,20 @@
+package com.dxc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan({"com.dxc"})
+public class Config {
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver vr = new InternalResourceViewResolver();
+		vr.setPrefix("/WEB-INF/jsp/");
+		vr.setSuffix(".jsp");
+		
+		return vr;
+	}
+}
